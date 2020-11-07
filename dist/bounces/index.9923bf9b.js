5b9cff7f262e1f84325f9766fad32569db74363c
Emma Goodliffe <emma@piemag.com>
2020-11-07 12:57:05 +0000

    downgraded parcel

--- dist/bounces/index.9923bf9b.js
+++
@@ -1,2 +0,0 @@
-!function(){var n=parcelRequire("1nRnE");var e,a,u=new((e=n())&&e.__esModule?e.default:e)((function(){}));u.setup=function(){u.createCanvas(600,600)},u.draw=function(){a.update(),a.draw()}}();
-//# sourceMappingURL=index.9923bf9b.js.map
